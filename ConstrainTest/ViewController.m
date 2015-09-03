9032a813b26af7d39298d665fa00ed2ceb686dc2
Jiamao Zheng <jiamao@emergemedia.com>
2015-09-03 09:24:19 -0500

    fdsfsdafsd

--- ConstrainTest/ViewController.m
+++ ConstrainTest/ViewController.m
@@ -16,7 +16,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    UIView *contentView = [[UIView alloc] init];
+//    contentView.backgroundColor = [UIColor whiteColor];
+//    self.view = contentView;
+    
+    UIView *centerView = [[UIView alloc] init];
+    [centerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    centerView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:centerView];
+    
+    // Width constraint, half of parent view width
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:centerView
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:0.4
+                                                           constant:0]];
+    
+    // Height constraint, half of parent view height
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:centerView
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:0.2
+                                                           constant:0]];
+    
+    // Center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:centerView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
+    // Center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:centerView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
